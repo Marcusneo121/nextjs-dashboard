ad81c4d95b67e5912b82f1673dd75ce858d4dd56
Marcus Neo <marcus121neo@gmail.com>
2024-03-09 14:19:03 +0800

    next auth secret

--- auth.config.ts
+++ auth.config.ts
@@ -18,4 +18,6 @@ export const authConfig = {
         },
     },
     providers: [], // Add providers with an empty array for now
+    secret: process.env.NEXTAUTH_SECRET,
+
 } satisfies NextAuthConfig;
